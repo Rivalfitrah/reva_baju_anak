88fc7c57b24c897bd7d95e4729de38992bfe2f57
Rivalfitrah <146603652+Rivalfitrah@users.noreply.github.com>
2025-04-21 08:46:18 +0700

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../app.css'
+import '../App.css'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import Card from '../components/Card'
