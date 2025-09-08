6dc3bf1e8b6a9ec95c940fb1dbac868113b045f7
rival fitrah <rivalfitrah9@gmail.com>
2025-09-08 20:18:34 +0700

    update detail

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const api = "http://backend-go-gin-production.up.railway.app"
+const api = "https://backend-go-gin-production.up.railway.app"
 
 export async function loginUser(email, password) {
     try {
